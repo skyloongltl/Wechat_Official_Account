276e24cf3b7ac3c8d7a6d54bb60207053e6a1e5c
skyloongltl <skyloongltl@gmail.com>
2017-08-31 20:46:56 +0800

    将->MsgId从对象转换为字符串

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-/*define('DIR', dirname(__FILE__));
+define('DIR', dirname(__FILE__));
 include DIR . "/app/AutoLoading.php";
 spl_autoload_register(array('\app\AutoLoading', 'autoload'));
-\app\Controller::run();*/
-$xml = file_get_contents("php://input");
-var_dump($xml->MsgId);
+\app\Controller::run();
